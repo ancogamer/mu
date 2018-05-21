55544fd89ee50fe50600d17d64babf5a9d897e2a
julioc98 <julioc.santos98@gmail.com>
2018-05-21 15:10:04 -0300

    add: SecurityMiddleware X-Client-ID Header

--- client/security.go
+++ client/security.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/fiscaluno/pandorabox"
@@ -9,17 +10,29 @@ import (
 // SecurityMiddleware log all requests URI
 func SecurityMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Do stuff here
-		// log.Println("METHOD:", r.Method, "|", "PATH:", r.RequestURI)
 
 		msg := pandorabox.Message{
-			Content: "security",
-			Status:  "OK",
+			Content: "Security",
+			Status:  "Unauthorized",
 			Body:    nil,
 		}
-		pandorabox.RespondWithJSON(w, http.StatusOK, msg)
-		return
-		// Call the next handler, which can be another middleware in the chain, or the final handler.
+
+		clientID := pandorabox.GetOSEnvironment("CLIENT_ID", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImNsaWVudCI6IkpDIn0sImlzcyI6Im11In0.spG-5_yioatRlT_em0i0p7XUP3J5Ne9XxiZL-GRK16Y")
+
+		if r.Header.Get("X-Client-ID") == "" {
+			log.Println("X-Client-ID is NULL")
+			pandorabox.RespondWithJSON(w, http.StatusUnauthorized, msg)
+			return
+		}
+
+		if r.Header.Get("X-Client-ID") != clientID {
+			log.Println("X-Client-ID " + r.Header.Get("X-Client-ID") + " don't matching")
+			pandorabox.RespondWithJSON(w, http.StatusUnauthorized, msg)
+			return
+		}
+
+		log.Println("X-Client-ID is " + r.Header.Get("X-Client-ID"))
+
 		next.ServeHTTP(w, r)
 	})
 }
